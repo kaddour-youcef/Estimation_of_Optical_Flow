67fd75855e2b066667d4d19d3f22abd74a8efecf
youcef kaddour <75435884+kaddour-youcef@users.noreply.github.com>
2020-12-17 14:43:28 +0100

    Update CalculeGradiant.m

--- CalculeGradiant.m
+++ CalculeGradiant.m
@@ -1,4 +1,8 @@
+% 3 methodes to estimate the derivatives
+
+
 function [fx, fy, ft] = CalculeGradiant(im1, im2)
+
 if size(im2,1)==0
     im2=zeros(size(im1));
 end
@@ -13,6 +17,7 @@ fx= conv2(im1,(1/12)*[-1 8 0 -8 1],'same');
 fy= conv2(im1,(1/12)*[-1 8 0 -8 1]','same');
 ft = conv2(im1, 0.25*ones(2),'same') + conv2(im2, -0.25*ones(2),'same');
 fx=-fx;fy=-fy;
+
 % methode des "simple finit masks"
 % fx = conv2(im1,[1 -1]);
 % fy = conv2(im1,[1; -1]);
